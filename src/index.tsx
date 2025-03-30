d3323b76e94a56dc5d3192be4924b19028b63e80
Alexander Evstafiadi <58134475+a1exevs@users.noreply.github.com>
2025-03-30 15:06:05 +0300

    [Refactoring] SCSS instead CSS (#8)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import 'src/index.css';
+import 'src/index.scss';
 import App from 'src/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
