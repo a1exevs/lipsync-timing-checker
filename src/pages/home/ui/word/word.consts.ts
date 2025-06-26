2f2012a3adb9415d460b875fb1d2f6099f6356a2
Alexander Evstafiadi <58134475+a1exevs@users.noreply.github.com>
2025-06-26 20:45:10 +0300

    [Improvement] Moving optimization (#30)

--- src/pages/home/ui/word/word.consts.ts
+++ src/pages/home/ui/word/word.consts.ts
@@ -4,6 +4,4 @@ export const WORD_CHAIN_RESIZER_COLOR = 'green';
 
 export const WORD_RESIZER_Z_INDEX = 1;
 export const WORD_RESIZER_WIDTH_PX = 5;
-export const WORD_RESIZER_WIDTH = `${WORD_RESIZER_WIDTH_PX}px`;
 export const WORD_MIN_WIDTH_PX = 2 * WORD_RESIZER_WIDTH_PX;
-export const WORD_MOVING_SENSITIVITY = 0.05;
