b51b810f9918a28964156a5732dc0dced57d412f
AlexEvs <aleksandrevstafiadi@gmail.com>
2025-07-22 09:12:12 +0300

    upd: build fix

--- src/shared/model/index.ts
+++ src/shared/model/index.ts
@@ -1,15 +1,11 @@
-import { arrayToObject } from 'src/shared/model/helpers/arrays';
-import { getExtensionByFile, getFileData } from 'src/shared/model/helpers/files';
-import { throttleTime } from 'src/shared/model/helpers/intervals';
-import { capitalize, capitalizeLabel } from 'src/shared/model/helpers/strings';
-import { currentLang } from 'src/shared/model/lang/lang.helpers';
-import { KeyOfValue } from 'src/shared/model/types/common';
-
 // helpers
-export { arrayToObject, getFileData, getExtensionByFile, throttleTime, capitalizeLabel, capitalize };
+export { arrayToObject } from 'src/shared/model/helpers/arrays';
+export { getExtensionByFile, getFileData } from 'src/shared/model/helpers/files';
+export { throttleTime } from 'src/shared/model/helpers/intervals';
+export { capitalize, capitalizeLabel } from 'src/shared/model/helpers/strings';
 
 // lang
-export { currentLang };
+export { currentLang } from 'src/shared/model/lang/lang.helpers';
 
 // types
-export type { KeyOfValue };
+export type { KeyOfValue } from 'src/shared/model/types/common';
